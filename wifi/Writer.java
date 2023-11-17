19a30c0d8184b1ee9f443220a9fbd9cafad01dff
jmkelsey <123215430+jmkelsey@users.noreply.github.com>
2023-11-17 15:01:42 -0800

    Update Writer.java
    added state machine logic

--- wifi/Writer.java
+++ wifi/Writer.java
@@ -6,91 +6,152 @@ import java.util.concurrent.ArrayBlockingQueue;
 /**
  * A thread that creates and sends an 802.11 frame with the specified data
  *
- * @author Jonah Kelsey and Mitchell Hurley
+ * @author Jonah Kelsey
  * @version 1.0 11/6/23
  */
 public class Writer implements Runnable
 {
-	private ArrayBlockingQueue<TransmissionData> queue;
     private RF theRF;
-    private PrintWriter output;
+    private ArrayBlockingQueue<Frame> sendQueue;
+    private ArrayBlockingQueue<Frame> ackQueue;
     private short ourMAC;
-    
-    public Writer(ArrayBlockingQueue<TransmissionData> queue,RF theRF, PrintWriter output, short ourMAC){
+    private PrintWriter output;
+
+    // Some timings
+    final int SIFS = 100;
+    final int SLOT = 200;
+    boolean retransmit;
+    final int DIFS = 20000;
+    final int defaultTimeout = 2000;
+
+
+    // Represents the state of our sender thread
+    private String state;
+    // How many retries we have done on current transmission
+    private int retries;
+    // Our current frame to be transmitted
+    private Frame currentFrame;
+    // Window properties
+    private int collisionWindow;
+    // Slot number
+    private int slotNum = 0;
+
+    public Writer(RF theRF, ArrayBlockingQueue<Frame> sendQueue, ArrayBlockingQueue<Frame> ackQueue, short ourMAC, PrintWriter output){
         this.theRF = theRF;
-        this.output = output;
+        this.sendQueue = sendQueue;
+        this.ackQueue = ackQueue;
         this.ourMAC = ourMAC;
-        this.queue = queue;
+        this.output = output;
+
+        state = "Await data";
+        retransmit = false;
+        retries = 0;
+        currentFrame = null;
+        collisionWindow = theRF.aCWmin;
     }
-    
+
+    //TODO finish
     @Override
     public void run(){
-        // Print
-    	while (true) {
-    		try {
-    			// Retrieve data from the queue
-                TransmissionData transmissionData = queue.take();
+        // Runs forever
+        while (true) {
+            // Test what state we are in
+            if (state == "Await data") {
+                retries = 0;
+                // Block until there is a message on queue, and then take it and send
+                try
+                {
+                    currentFrame = sendQueue.take();
+                }
+                catch (InterruptedException ie)
+                {
+                    ie.printStackTrace();
+                }
 
-                short dest = transmissionData.getDest();
-                byte[] data = transmissionData.getData();
-                int len = transmissionData.getLen();
-		        output.println("LinkLayer: Sending " + len + " bytes to " + dest);
-		        
-		        //TODO Maybe turn these into methods? like a buildFrame() and transmitFrame()
-		        // First build the frame
-		        byte[] frame = buildFrame(dest, data, len);
-		        int result = transmitFrame(frame);
-    		}catch(InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        
-        // If we made it here, there's a problem
-    }
-    /**
-     * Method that builds a frame after confirming the data is valid
-     * @return A byte array representing a frame of data to be transmitted
-     */
-    private byte[] buildFrame(short dest, byte[] data, int len) {
-    	
-    	//TODO check frame validity
-    	
-    	byte[] frame = new byte[len + 10]; // 16 bytes for MAC header
-        frame[0] = 0x0; // set frame type, retry, and sequence to 0
-        frame[1] = 0x0; // set frame type, retry, and sequence to 0
-        frame[2] = (byte) dest; // Destination MAC address (lower byte)
-        frame[3] = (byte) (dest >> 8); // Destination MAC address (upper byte)
-        frame[4] = (byte) ourMAC; // Source MAC address (lower byte)
-        frame[5] = (byte) (ourMAC >> 8); // Source MAC address (upper byte)
-        
-        // Copy the data into the frame
-        System.arraycopy(data, 0, frame, 6, len);
-        
-        // set crc to -1
-        frame[frame.length - 1] = (byte) 0xff;
-        frame[frame.length - 2] = (byte) 0xff;
-        frame[frame.length - 3] = (byte) 0xff;
-        frame[frame.length - 4] = (byte) 0xff;
-        return frame;
-    }
-    
-    private int transmitFrame(byte[] frame) {
-    	
-    	// This is an arbitrary loop, could be an endless while, but if we try 10k times there is probably something wrong, could be used for backoff later
-        for (int i = 0; i < 10000; i++) {
-            if (!theRF.inUse()) {
-                theRF.transmit(frame);
-                return 1;
-            } else {
-                // insert a delay or use a different mechanism to avoid busy-waiting.
+                // if the RF is not in use move to idle wait
+                if (!theRF.inUse()) {
+                    state = "Idle DIFS wait";
+                } else { // if the RF is in use move to busy wait
+                    state = "Busy DIFS wait";
+                }
+            } else if (state == "Idle DIFS wait") {
+                // We do our wait
                 try {
-                    Thread.sleep(10); // Sleep for 10 milliseconds
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                    Thread.sleep(DIFS);
+                }
+                catch (InterruptedException ex) {}
+                // Check if the channel is idle
+                if (theRF.inUse()) { // if it was in use move to busy wait
+                    state = "Busy DIFS wait";
+                } else { // if not transmit and await ack
+                    // TODO transmit
+                    state = "Await ACK";
+                }
+            } else if (state == "Busy DIFS wait") {
+                // wait for idle
+                while (theRF.inUse()) {
+                    // wait till free
+                }
+                // move to slot wait
+                state = "Slot wait";
+            } else if (state == "Slot wait") {
+                // wait for our number of slots
+                // TODO maybe not accurate? need to see if channel was idle while waiting?
+                for (int i = 0; i < slotNum; i++) {
+                    try {
+                        Thread.sleep(SLOT);
+                    }
+                    catch (InterruptedException ex) {}
+                }
+                // Check channel
+                if (theRF.inUse()) { // if busy 
+                    state = "Busy DIFS wait";
+                } else { // if not transmit and await ack
+                    // TODO transmit
+                    state = "Await ACK";
+                }
+            } else if (state == "Await ACK") {
+                // wait for timeout or ack arrival
+                int timeout = defaultTimeout;
+                while (ackQueue.peek() == null && timeout > 0) {
+                    try {
+                        Thread.sleep(20L);
+                    }
+                    catch (InterruptedException ex) {}
+                }
+                // Check ack queue
+                if (ackQueue.peek() != null) {
+                    // if we have an ack, remove from queue and reset
+                    collisionWindow = theRF.aCWmin;
+                    retries = 0;
+                    try
+                    {
+                        ackQueue.take();
+                    }
+                    catch (InterruptedException ie)
+                    {
+                        ie.printStackTrace();
+                    }
+                    state = "Await data";
+                } else {
+                    // check if we have reached rery limit
+                    if (retries >= theRF.dot11RetryLimit) {
+                        // if we have reached the limit, give up
+                        retries = 0;
+                        // TODO cw?
+                        state = "Await data";
+                    }
+                    retries++;
+                    
+                    // go to busy difs wait
+                    state = "Busy DIFS wait";
                 }
             }
+
         }
-		return -1;
     }
-    
+
+    private int transmit(Frame outgoingFrame) {
+        return 0;
+    }
 }
